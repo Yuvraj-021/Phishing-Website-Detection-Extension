aa1e3f472589e5e813ce3b0e6bae8263b755f4b8
Yuvraj-021 <yuvrajashokdeshmukh@example.com>
2024-05-28 22:56:27 +0530

    changing approach

--- phish-api/read.py
+++ phish-api/read.py
@@ -12,33 +12,19 @@ from keras.models import load_model
 from keras.utils import to_categorical
 from flask_cors import CORS
 import tensorflow as tf
+
+from sklearn import metrics 
+
+import pickle
+from feature import FeatureExtraction
+
 app = Flask(__name__)
 CORS(app)
-all_in_one_model = load_model('model_all.h5')
+import joblib
+gbc = joblib.load('model.pkl')
+joblib.dump(gbc, 'model.pkl')
 
 
-tokener = Tokenizer(lower=True, char_level=True, oov_token='-n-')
-
-# Add the following function to load the tokenizer with more data
-def load_tokenizer(filename):
-    tokener.load_weights(filename)
-
-# Add the following function to preprocess the URL using the tokenizer
-def preprocess_url(url):
-    tokener.fit_on_texts([url])
-    char_index = tokener.word_index
-    x_train = np.asanyarray(tokener.texts_to_sequences([url]))
-    x_train = pad_sequences(x_train, maxlen=200)
-    return x_train, char_index
-
-
-# Add the following function to make predictions using the Keras model
-def predict_keras(x_train, char_index):
-    #x_train = to_categorical(x_train, num_classes=len(char_index) + 1)
-    #prediction = all_in_one_model.predict(x_train).tolist()
-    #classes_x=np.argmax(prediction,axis=1)
-    prediction=np.argmax(all_in_one_model.predict(x_train),axis=1).tolist()
-    return prediction
 
 @app.route('/',methods=["GET","POST"])  
 def home():
@@ -46,16 +32,19 @@ def home():
 
 @app.route('/post',methods=['POST'])
 def predict():
-    url = request.form['URL']
-    x_train, char_index = preprocess_url(url)
-    #features = extract_features(url)
-    keras_prediction = predict_keras(x_train, char_index)
-    print(keras_prediction)
-    #return "0"
-    #svm_prediction = predict_svm(features)
-    if keras_prediction == 1:
+    # 
+    url = request.form["URL"]
+    obj = FeatureExtraction(url)
+    x = np.array(obj.getFeaturesList()).reshape(1,30) 
+    y_pred =gbc.predict(x)[0]
+    y_pro_phishing = gbc.predict_proba(x)[0,0]
+    
+    print("Y pred Pro ",y_pro_phishing)
+    print("Y pred",y_pred)
+    
+    if y_pred == 0:
         return "-1"
-    elif keras_prediction ==0:
+    elif y_pred ==1:
         return "0"
     else:
         return "1"
